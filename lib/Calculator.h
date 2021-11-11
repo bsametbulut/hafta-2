6e8ab2544a48b18c06f73be612bc795c244ce0e5
Basar-SametSame <93772890+bsametbulut@users.noreply.github.com>
2021-11-11 11:35:33 +0000

    Initial commit

---
+++ lib/Calculator.h
@@ -0,0 +1,17 @@
+class Calculator
+{
+  private 
+  double a,b,sonuc;
+
+public:
+Calculator();
+void toplama();
+void cıkarma();
+void carpma();
+void bolum();
+void seta(double);
+void setb(double);
+void getresult();
+
+
+};
